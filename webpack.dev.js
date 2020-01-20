f7855da927247cb5bfd3e542c2b5b93b6ab76414
Pierluigi Milo <piermilo@gmail.com>
2020-01-20 23:14:00 +0000

    initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+    const path = require("path");
+    const common = require("./webpack.common");
+    const merge = require("webpack-merge");
+    
+    
+    module.exports = merge(common, {
+    	mode: "development",
+    	output: {
+    		filename: "[name].bundle.js",
+    		path: path.resolve(__dirname, "dist")
+    	},
+    	plugins: [
+    		new HtmlWebpackPlugin({
+    			template: "./src/template.html"
+    		})
+    	],
+    	module: {
+    		rules: [
+    			{
+    				test: /\.css$/,
+    				use: [
+    							"style-loader", // 2. Inject styles into DOM
+    							"css-loader" // 1. Turns css into common js
+    				]
+    			}
+    		]
+    	}
+    });
